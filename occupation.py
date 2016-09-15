89ded438f98b8671b2746b59082a68922f1d31b0
ThisIsMyNick <ThisIsMyNick@users.noreply.github.com>
2016-09-15 12:30:49 -0400

    combine files

--- occupation.py
+++ occupation.py
@@ -1,11 +1,34 @@
 import random
 
-d = {
-    "Management" : 22.0,
-    "Business" : 31.6,
-    "Legal" : 1.4,
-    "Arts" : 44.9
-}
+f = open("occupations.csv", "r").readlines()
+f = [x.strip("\r\n") for x in f]
+f = f[1:-1]
+
+def parse(elem):
+    occ = ""
+    i = 0
+    while i < len(elem):
+        if elem[i] == '"':
+            occ += elem[i]
+            i += 1
+            while elem[i] != '"':
+                occ += elem[i]
+                i += 1
+            occ += '"'
+        elif elem[i] == ",":
+            i += 1
+            break
+        else:
+            occ += elem[i]
+        i += 1
+    pt = float(elem[i:])
+    return occ, pt
+
+
+d = {}
+for e in f:
+    occ,pt = parse(e)
+    d[occ] = pt
 
 def randOcc(dict):
     perc = random.random() * 100;
@@ -16,4 +39,3 @@ def randOcc(dict):
     return "Unoccupied"
 
 print(randOcc(d))
-        
